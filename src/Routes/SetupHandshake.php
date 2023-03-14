cd6acbad1c9e3b6c4792970c546ad742b34a28c1
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-03-14 13:10:24 +0100

    setup handshake

--- src/Routes/SetupHandshake.php
+++ src/Routes/SetupHandshake.php
@@ -4,83 +4,67 @@ namespace Tualo\Office\OnlineVote\Routes;
 use Tualo\Office\Basic\TualoApplication as App;
 use Tualo\Office\Basic\Route as BasicRoute;
 use Tualo\Office\Basic\IRoute;
+use Tualo\Office\OnlineVote\APIRequestHelper;
 use Tualo\Office\TualoPGP\TualoApplicationPGP;
 use Ramsey\Uuid\Uuid;
 
 class SetupHandshake implements IRoute{
+
+    public static function remoteLogin(){
+        $cookie_file = App::get('tempPath').'/api_cookie';
+        if (file_exists($cookie_file)) unlink($cookie_file);
+
+        if ($api_result = APIRequestHelper::query( $_REQUEST['api_url'], [
+            'username' => $_REQUEST['api_username'],
+            'password' => $_REQUEST['api_password'],
+            'mandant' => $_REQUEST['api_client'],
+            'forcelogin' => '1'
+        ] )){
+            if ($api_result['success']==false) throw new \Exception($api_result['msg'].'-');
+            return true;
+        }
+        return false;
+    }
  
     public static function register(){
         BasicRoute::add('/onlinevote/setuphandshake',function($matches){
             try{
                 App::contenttype('application/json');
-                
+
                 $session = App::get('session');
                 $db = $session->getDB();
 
-                $token = $session->registerOAuth(
-                        $params     =   [],
-                        $force      =   true,
-                        $anyclient  =   false,
-                        $path       =   '/onlinevote/*'
-                );
+                if (self::remoteLogin()===true){
 
-                $session->oauthValidDays($token,365);
+                    $token = $session->registerOAuth(
+                            $params     =   [],
+                            $force      =   true,
+                            $anyclient  =   false,
+                            $path       =   '/onlinevote/*'
+                    );
+                    $session->oauthValidDays($token,365);
+                    $keys = TualoApplicationPGP::keyGen(2048);
+                    $publickey = $keys['public'];
 
-
-                $keys = TualoApplicationPGP::keyGen(2048);
-                $publickey = $keys['public'];
-
-                $mesage_to_send = [
-                    'publickey' => $publickey,
-                    'domain'    => $_REQUEST['domain'],
-                    'token'     => $token,
-                    'message'   => TualoApplicationPGP::encrypt($publickey,'OK MESSAGE')
-                ];
-                App::result('data',  $mesage_to_send );
-                
-                /*
-                
-                $keyData = $db->singleRow("select publickey from wm_pgp_pubkey where id=0 ",[]);
-
-                $sql='select * from view_voters_by_username_api where username={username}';
-                App::contenttype('application/json');
-                $data = $db->singleRow($sql,$matches);
-
-                if ($data !== false){
-                    if (is_string($data['possible_ballotpapers'])){
-                        $data['possible_ballotpapers'] = json_decode($data['possible_ballotpapers'],true);
-                    }
-
+                    $mesage_to_send = [
+                        'publickey' => $publickey,
+                        'domain'    => $_SERVER['SERVER_NAME'],
+                        'uri'       => substr($_SERVER['SCRIPT_URI'],0,-1*strlen('/onlinevote/setuphandshake')),
+                        'token'     => $token,
+                        'message'   => TualoApplicationPGP::encrypt($publickey,$token)
+                    ];
                     
-                    foreach( $data['possible_ballotpapers'] as &$ballotpaper){
-                        $hash = [
-                            'voter_id'=>$ballotpaper['voter_id'],
-                            'ballotpaper_ridx' => $ballotpaper['ballotpaper_id'].'|0',
-                            'secret_token' => Uuid::uuid4()->toString()
-                        ];
-                        if ($ballotpaper['canvote']==1){
-                            $db->direct('
-                            insert into wm_wahlschein_register (
-                                id,
-                                stimmzettel,
-                                createdate,
-                                token
-                            ) values (
-                                {voter_id},
-                                {ballotpaper_ridx},
-                                now(),
-                                {secret_token}
-                            ) on duplicate key update token=values(token)
-                            ',$hash);
-                        }
-                        $ballotpaper['secret_token'] = TualoApplicationPGP::encrypt($keyData['publickey'],$hash['secret_token']);
+                    if ($api_result = APIRequestHelper::query( $_REQUEST['api_url'].'/papervote/setuphandshake', $mesage_to_send )){
+                        if (TualoApplicationPGP::decrypt($privatekey,$api_result['message'])!=$token) throw new \Exception('Problem bei dem Schlüsseltausch');
+
                     }
+    
                 }
 
-                App::result('data',  $data );
-                App::result('success',$data !== false);
-                App::result('msg',($data === false)?'Der Wähler wurde nicht gefunden.':'');
-                */
+
+                
+                App::result('data',  $mesage_to_send );
+                
             }catch(\Exception $e){
 
                 App::result('last_sql', $db->last_sql);
