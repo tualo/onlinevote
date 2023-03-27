0fdda6c9cc2287e3612db7b76d140c23452d4b92
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-03-27 15:06:03 +0200

    clean code

--- src/States/Error.php
+++ src/States/Error.php
@@ -5,13 +5,11 @@ use Tualo\Office\OnlineVote\States\State;
 use Tualo\Office\OnlineVote\WMStateMachine;
 
 class Error implements State{
-
     public function prepare(&$request,&$result):string {
         $stateMachine = WMStateMachine::getInstance();
         $stateMachine->voter(true);
         return $stateMachine->getNextState();
     }
-
     public function transition(&$request,&$result):string {
         $stateMachine = WMStateMachine::getInstance();
         if (($logoutState = $stateMachine->checkLogout())!='') return $logoutState;
