dc652366cf3c410aff1f29e889623f95763e57a6
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-10-06 14:03:39 +0200

    fixes

--- src/sql/view_website.ballotpaper.sql
+++ src/sql/view_website.ballotpaper.sql
@@ -8,18 +8,6 @@ stimmzettel.id ballotpaper_id
 from stimmzettelgruppen join stimmzettel on stimmzettelgruppen.stimmzettel=stimmzettel.ridx;
 
 
-create index idx_ds_renderer_stylesheet_classname on ds_renderer_stylesheet(classname);
-alter table ds_renderer_stylesheet drop primary key;
-alter table ds_renderer_stylesheet add primary key(classname,`group`);
-
-
-create index idx_ds_renderer_stylesheet_attributes_classname_attribute on ds_renderer_stylesheet(classname,attribute);
-alter table  ds_renderer_stylesheet_attributes drop primary key;
-alter table  ds_renderer_stylesheet_attributes add `group` integer;
-update ds_renderer_stylesheet_attributes set `group` = (select max(`group`) from  ds_renderer_stylesheet where ds_renderer_stylesheet.classname = ds_renderer_stylesheet_attributes.classname) where `group` is null;
-alter table  ds_renderer_stylesheet_attributes add primary key(classname,attribute,`group`);
-
-
 
 create table unique_voter_session (
 id varchar(36) primary key,
@@ -27,6 +15,7 @@ session_id varchar(50),
 create_time datetime default current_timestamp
 );
 
+delimiter //
 
 CREATE OR REPLACE TRIGGER voters_bi_completed
 AFTER INSERT
@@ -35,25 +24,7 @@ BEGIN
   IF EXISTS(select voter_id from voters where voter_id=new.voter_id and stimmzettel=new.stimmzettel and completed=1) THEN
     SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'allready voted';
   END IF;
-END
-
-
-
-
-alter table wm_wahlschein_register drop wahlscheinnummer;
-alter table wm_wahlschein_register drop birthdate_year;
-alter table wm_wahlschein_register drop birthdate_month;
-alter table wm_wahlschein_register drop birthdate_day;
-alter table wm_wahlschein_register drop phone_lc;
-alter table wm_wahlschein_register drop phone_number;
-alter table wm_wahlschein_register drop pin;
-alter table wm_wahlschein_register drop sms_response;
-alter table wm_wahlschein_register drop tmg_token;
-alter table wm_wahlschein_register drop person;
-
-drop trigger wm_wahlschein_register__au;
-drop trigger wm_wahlschein_register__ai;
-drop trigger wm_wahlschein_register__bd;
+END //
 
 
 
@@ -80,8 +51,9 @@ BEGIN
     END IF;
   END IF;
   RETURN (lvl);
-END
+END //
 
+delimiter ; 
 
 insert into  system_settings (system_settings_id) values 
 	('remote-erp/public'),
