64cc2014737b4b7563ca273797900e356715d10a
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-10-09 10:20:09 +0200

    fixes

--- src/js/classic/routes/OnlineVote.js
+++ src/js/classic/routes/OnlineVote.js
@@ -5,8 +5,10 @@ Ext.define('Tualo.routes.OnlineVote',{
             Ext.getApplication().addView('Tualo.OnlineVote.Viewport');
         },
         before: function ( action) {
+            /*
             let ping = Ext.getApplication().sessionPing;
             if (ping.success===true) action.stop();
+            */
             action.resume();
         }
     }
