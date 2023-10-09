a831cdac23474c9700317af788fec086c351d1ad
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-10-09 10:08:32 +0200

    fix sql

--- src/sql/install/pgpkeys.readtable.sql
+++ src/sql/install/pgpkeys.readtable.sql
@@ -1,6 +1,60 @@
-delimiter ;
+delimiter;
 
-CREATE OR REPLACE VIEW `view_readtable_pgpkeys` AS
+CREATE OR REPLACE VIEW `view_readtable_pgpkeys_valid` AS
+select
+    `a`.`keyname` AS `keyname`,
+    cast(
+        sum(if(`a`.`x` <> `a`.`hash_value`, 1, 0)) as signed
+    ) AS `invalid`,
+    count(0) AS `total`,
+    sum(`a`.`blocked`) AS `blocked`,
+    sum(`a`.`encrypted`) AS `encrypted`
+from
+    (
+        select
+            `ballotbox`.`keyname` AS `keyname`,
+            `ballotbox`.`id` AS `id`,
+            `ballotbox`.`blocked` AS `blocked`,
+            `ballotbox`.`voter_id` AS `voter_id`,
+            md5(
+                concat(
+                    `ballotbox_blockchain`.`last_hash`,
+                    md5(`ballotbox`.`ballotpaper`)
+                )
+            ) AS `x`,
+            `ballotbox_blockchain`.`id` AS `bbid`,
+            `ballotbox_blockchain`.`hash_value` AS `hash_value`,
+            if(`ballotbox_encrypted`.`id` is null, 0, 1) AS `encrypted`
+        from
+            (
+                (
+                    `ballotbox_blockchain`
+                    join `ballotbox` on(
+                        (
+                            `ballotbox_blockchain`.`ballotpaper_id`,
+                            `ballotbox_blockchain`.`keyname`
+                        ) = (`ballotbox`.`id`, `ballotbox`.`keyname`)
+                    )
+                )
+                left join `ballotbox_encrypted` on(
+                    (`ballotbox`.`id`, `ballotbox`.`keyname`) = (
+                        `ballotbox_encrypted`.`id`,
+                        `ballotbox_encrypted`.`keyname`
+                    )
+                )
+            )
+        where
+            `ballotbox`.`saveerror` = 0
+        group by
+            `ballotbox`.`keyname`,
+            `ballotbox`.`id`
+    ) `a`
+group by
+    `a`.`keyname` 
+;
+
+CREATE
+    OR REPLACE VIEW `view_readtable_pgpkeys` AS
 select
     `pgpkeys`.`keyname` AS `keyname`,
     `pgpkeys`.`keyid` AS `keyid`,
@@ -25,4 +79,3 @@ from
             `view_readtable_pgpkeys_valid`.`keyname` = `pgpkeys`.`keyname`
         )
     );
-    
