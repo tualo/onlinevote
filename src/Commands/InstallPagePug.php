6edc8f1fff8b99e859c95008e24409b0a1900150
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-10-17 11:52:38 +0200

    add page pug

---
+++ src/Commands/InstallPagePug.php
@@ -0,0 +1,77 @@
+<?php
+namespace Tualo\Office\OnlineVote\Commands;
+use Garden\Cli\Cli;
+use Garden\Cli\Args;
+use phpseclib3\Math\BigInteger\Engines\PHP;
+use Tualo\Office\Basic\ICommandline;
+use Tualo\Office\ExtJSCompiler\Helper;
+use Tualo\Office\Basic\TualoApplication as App;
+use Tualo\Office\Basic\PostCheck;
+
+class InstallPagePug implements ICommandline{
+
+    public static function getCommandName():string { return 'install-sql-onlinevote-pug';}
+
+    public static function setup(Cli $cli){
+        $cli->command(self::getCommandName())
+            ->description('installs basic pug templatessql for onlinevote module')
+            ->opt('client', 'only use this client', true, 'string');
+            
+    }
+
+   
+    public static function setupClients(string $msg,string $clientName,string $file,callable $callback){
+        $_SERVER['REQUEST_URI']='';
+        $_SERVER['REQUEST_METHOD']='none';
+        App::run();
+
+        $session = App::get('session');
+        $sessiondb = $session->db;
+        $dbs = $sessiondb->direct('select username dbuser, password dbpass, id dbname, host dbhost, port dbport from macc_clients ');
+        foreach($dbs as $db){
+            if (($clientName!='') && ($clientName!=$db['dbname'])){ 
+                continue;
+            }else{
+                App::set('clientDB',$session->newDBByRow($db));
+                PostCheck::formatPrint(['blue'],$msg.'('.$db['dbname'].'):  ');
+                $callback($file);
+                PostCheck::formatPrintLn(['green'],"\t".' done');
+
+            }
+        }
+    }
+
+    public static function run(Args $args){
+
+        $files = [
+            'page/pug'    => 'setup pug templates',
+        ];
+        
+
+        foreach($files as $file=>$msg){
+            $installSQL = function(string $file){
+
+                $filename = dirname(__DIR__).'/sql/'.$file.'.sql';
+                $sql = file_get_contents($filename);
+                $sql = preg_replace('!/\*.*?\*/!s', '', $sql);
+                $sql = preg_replace('#^\s*\-\-.+$#m', '', $sql);
+
+                $sinlgeStatements = App::get('clientDB')->explode_by_delimiter($sql);
+                foreach($sinlgeStatements as $commandIndex => $statement){
+                    try{
+                        App::get('clientDB')->execute($statement);
+                        App::get('clientDB')->moreResults();
+                    }catch(\Exception $e){
+                        echo PHP_EOL;
+                        PostCheck::formatPrintLn(['red'], $e->getMessage().': commandIndex => '.$commandIndex);
+                    }
+                }
+            };
+            $clientName = $args->getOpt('client');
+            if( is_null($clientName) ) $clientName = '';
+            self::setupClients($msg,$clientName,$file,$installSQL);
+        }
+
+
+    }
+}
