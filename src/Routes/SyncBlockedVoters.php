5f3d88daea51b10bad0817e7b8fb9ab3e3aee24d
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-11-29 10:53:21 +0100

    fix sync

--- src/Routes/SyncBlockedVoters.php
+++ src/Routes/SyncBlockedVoters.php
@@ -26,7 +26,18 @@ class SyncBlockedVoters implements IRoute
                 $db->autocommit(false);
                 $db->direct('delete from blocked_voters');
                 $tablename = 'blocked_voters';
-                $url = $db->singleValue("select property v FROM erp_sync_url WHERE id = 1  ", [], 'v');
+
+                $o = $db->directMap("
+                    select if(property<>'',1,0) v,'api' text FROM system_settings WHERE system_settings_id = 'remote-erp/url' 
+                    union 
+                    select property v,'api_url' text FROM system_settings WHERE system_settings_id = 'remote-erp/url'
+                    union
+                    select property v,'api_token' text FROM system_settings WHERE system_settings_id = 'remote-erp/token'
+                    union
+                    select property v,'api_private' text FROM system_settings WHERE system_settings_id = 'erp/privatekey'
+                ",[],'text','v');
+
+                $url = $o['remote-erp/url'];
                 $blocked_voters = APIRequestHelper::query($url . '/ds/' . $tablename . '/read?limit=1000000');
                 if ($blocked_voters === false) throw new Exception("Fehler beim Abrufen der blockierten Wähler (" . APIRequestHelper::$last_error_message . ")");
                 $res = DSCreateRoute::createRequest($db, $tablename, $blocked_voters);
