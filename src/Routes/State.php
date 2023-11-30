a5df336c355bffebc6a62ca5b6f9cf2c1943f61c
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-11-30 09:04:16 +0100

    add time

--- src/Routes/State.php
+++ src/Routes/State.php
@@ -13,9 +13,16 @@ class State implements IRoute{
             App::contenttype('application/json');
             App::result('success',false);
             try{
+                $session = App::get('session');
+                $db = $session->getDB();
+
                 $param = Handshake::parameter();
                 App::result('starttime',isset($param['starttime'])?$param['starttime']:null);
                 App::result('stoptime',isset($param['stoptime'])?$param['stoptime']:null);
+                App::result('timezone',date_default_timezone_get());
+                App::result('php_time',date('Y-m-d H:i:s'));
+                App::result('db_time',$db->direct('select now() as n',[],'n'));
+
 
                 Handshake::pingRemote() || throw new \Exception('Der Remote Server ist nicht erreichbar');
                 App::result('success',true);
