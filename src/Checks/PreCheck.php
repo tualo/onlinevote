9471adce20c5aedb8f230012305d2d25072cbe6a
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2024-01-15 10:46:50 +0100

    tests

--- src/Checks/PreCheck.php
+++ src/Checks/PreCheck.php
@@ -11,8 +11,10 @@ class PreCheck  extends PCheck {
     public static function test(array $config){
         $clientdb = App::get('clientDB');
         if (is_null($clientdb)) return;
-        if ($clientdb->singleRow('select stoptime from wm_loginpage_settings where starttime<now() and id = 1', []) === false) {
+        if ($clientdb->singleRow('select starttime from wm_loginpage_settings where starttime<now() and id = 1', []) === false) {
             throw new \Exception("Es kann nur vor dem Start der Wahlfrist aktualisiert werden");
+        }else{
+            PreCheck::formatPrintLn(['green'],'onlinevote, starttime safe');
         }
         // exit(65);
     }
