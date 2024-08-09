01a8c1ced07325d0fe65384d273aa54dbeccf252
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2024-08-09 10:28:13 +0200

    add setup command

---
+++ src/Commands/Setup.php
@@ -0,0 +1,70 @@
+<?php
+namespace Tualo\Office\OnlineVote\Commands;
+use Garden\Cli\Cli;
+use Garden\Cli\Args;
+use phpseclib3\Math\BigInteger\Engines\PHP;
+use Tualo\Office\Basic\ISetupCommandline;
+use Tualo\Office\ExtJSCompiler\Helper;
+use Tualo\Office\Basic\TualoApplication as App;
+use Tualo\Office\Basic\PostCheck;
+use Tualo\Office\Basic\CommandLineInstallSessionSQL;
+
+class Setup implements ISetupCommandline{
+
+    public static function getCommandName(): string { return 'onlinevote'; }
+    public static function getCommandDescription(): string { return 'perform a complete onlinevote setup'; }
+    public static function setup(Cli $cli){
+        $cli->command(self::getCommandName())
+            ->description(self::getCommandDescription())
+            ->opt('client', 'only use this client', true, 'string');
+    }
+    public static function run(Args $args) { 
+        $clientName = $args->getOpt('client');
+        if( is_null($clientName) ) $clientName = '';
+        
+        PostCheck::formatPrintLn(['blue'], "Installing all needed SQL procedures for onlinevote module");
+        PostCheck::formatPrintLn(['blue'], "==========================================================");
+        
+        $installCommands = [
+            'install-sessionsql-bsc-main',
+            'install-sql-sessionviews',
+            'install-sql-bsc-main-ds',
+            'install-sql-bsc-menu',
+            'install-sql-ds-main',
+            'install-sql-ds',
+            'install-sql-ds-dsx',
+            'install-sql-ds-privacy',
+            'install-sql-ds-docsystem',
+            'install-sql-tualojs',
+            'install-sql-monaco',
+            'install-sql-dashboard',
+            'install-sql-bootstrap',
+            'configuration --section scss --key cmd --value $(which sass)',
+            'import-bootstrap-scss',
+            'install-sql-bootstrap-menu',
+            'install-sql-cms',
+            'install-sql-cms-menu',
+            'install-sql-onlinevote',
+            'import-onlinevote',
+            'import-onlinevote-page'
+        ];
+        foreach($installCommands as $cmdString){
+            self::performInstall($cmdString,$clientName);
+        }
+
+    }
+
+    public static function performInstall(string $cmdString,string $clientName) {
+        $cmd = explode(' ',$cmdString);
+        $cmd[] = '--client='.$clientName;
+        $classes = get_declared_classes();
+        foreach($classes as $cls){
+            $class = new \ReflectionClass($cls);
+            if ( $class->implementsInterface('Tualo\Office\Basic\ICommandline') ) {
+                if($cmd[0]==$cls::getCommandName()){
+                    $cls::run(['./tm',...$cmd]);
+                }
+            }
+        }
+    }
+}
--- src/functions.php
+++ src/functions.php
@@ -9,6 +9,7 @@ require_once "Commands/InstallMainSQLCommandline.php";
 require_once "Commands/InstallMenu.php";
 require_once "Commands/InstallPagePug.php";
 require_once "Commands/Import.php";
+require_once "Commands/Setup.php";
 
 require_once "CMSMiddleware/Init.php";
 require_once "Middlewares/Middleware.php";
