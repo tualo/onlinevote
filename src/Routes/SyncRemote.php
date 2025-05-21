b8a534ce7f9fe6e9f7fbeeb4e9ced14448c2ee2f
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-05-21 11:03:19 +0200

    write css

--- src/Routes/SyncRemote.php
+++ src/Routes/SyncRemote.php
@@ -13,6 +13,25 @@ use Tualo\Office\DS\DSTable;
 
 class SyncRemote implements IRoute
 {
+    private static function writeBallotPaperCSS()
+    {
+        $session = TualoApplication::get('session');
+        $db = $session->getDB();
+        $sql = "select group_concat(concat('.ballot-background-',id,'{background-color:',farbe,char(59),'}') separator 'SLASH_N') x from stimmzettel";
+        $result = $db->singleValue($sql, [], 'x');
+        $result = str_replace('SLASH_N', "\n", $result);
+        $filename = 'ballotpaper-backgrounds.css';
+        $publicpath =  TualoApplication::configuration(
+            'tualo-cms',
+            'public_path'
+        );
+        if ($publicpath !== false) {
+            $publicpath = rtrim($publicpath, '/');
+            $filename = $publicpath . '/' . $filename;
+            file_put_contents($filename, $result);
+        }
+    }
+
 
     public static function register()
     {
@@ -29,18 +48,28 @@ class SyncRemote implements IRoute
                     select property v,'api_token' text FROM system_settings WHERE system_settings_id = 'remote-erp/token'
                     union all
                     select property v,'api_private' text FROM system_settings WHERE system_settings_id = 'erp/privatekey'
-                ",[],'text','v');
-                
+                ", [], 'text', 'v');
+
                 $url = $_SERVER['SERVER_NAME'];
                 if (isset($o['api_url'])) $url = $o['api_url'];
                 TualoApplication::result('url', $url);
-                TualoApplication::result('token',  isset($o['api_token'])? $o['api_token']:'');
+                TualoApplication::result('token',  isset($o['api_token']) ? $o['api_token'] : '');
 
                 TualoApplication::result('success', true);
             } catch (\Exception $e) {
                 TualoApplication::result('msg', $e->getMessage());
             }
         }, ['get', 'post'], true);
+
+        BasicRoute::add('/onlinevote/writecss', function () {
+            TualoApplication::contenttype('application/json');
+            try {
+                SyncRemote::writeBallotPaperCSS();
+                TualoApplication::result('success', true);
+            } catch (\Exception $e) {
+                TualoApplication::result('msg', $e->getMessage());
+            }
+        }, ['get'], true);
 
         BasicRoute::add('/onlinevote/syncremote', function () {
             TualoApplication::contenttype('application/json');
@@ -49,7 +78,7 @@ class SyncRemote implements IRoute
                 ini_set('memory_limit', '4096M');
 
                 TualoApplication::result('state', __LINE__);
-                TualoApplication::result('seconds',time() - $start);
+                TualoApplication::result('seconds', time() - $start);
                 $session = TualoApplication::get('session');
                 $config = TualoApplication::get('configuration');
                 $db = $session->getDB();
@@ -65,58 +94,57 @@ class SyncRemote implements IRoute
                 ", [], 'text', 'v');
                 $url  = $o['api_url'] . '~/' . $o['api_token'] . '/';
                 TualoApplication::result('state', __LINE__);
-                TualoApplication::result('seconds',time() - $start);
-               
+                TualoApplication::result('seconds', time() - $start);
+
                 $remote_data = [];
                 $table_list = $db->direct('select table_name from wm_sync_tables order by position asc');
                 foreach ($table_list as $table_row) {
                     set_time_limit(300);
                     $tablename = $table_row['table_name'];
 
-                    if ($tablename == 'ds_files_data'){
+                    if ($tablename == 'ds_files_data') {
                         // nicht einlesen, da daten zu groß sein können
-                    }else{
-                        $remote_data[$tablename] = APIRequestHelper::query($url . '/ds/' . $tablename . '/read?limit=1000000');
-                        if($remote_data[$tablename]===false){
-                            throw new \Exception('error on '.$tablename.' '.APIRequestHelper::$last_error_message);
+                    } else {
+                        $remote_data[$tablename] = APIRequestHelper::query($url . '/papervote/' . $tablename . '/read?limit=1000000');
+                        if ($remote_data[$tablename] === false) {
+                            throw new \Exception('error on ' . $tablename . ' ' . APIRequestHelper::$last_error_message);
                         }
                     }
                     $db->direct('select table_name from `ds` limit 1');
                 }
                 TualoApplication::result('state', __LINE__);
-                TualoApplication::result('seconds',time() - $start);
+                TualoApplication::result('seconds', time() - $start);
 
                 $db->autocommit(false);
                 foreach ($table_list as $table_row) {
-                    if ($table_row['table_name'] == 'ds_files'){
+                    if ($table_row['table_name'] == 'ds_files') {
 
-                        $run=true;
-                        $run_index=0;
-                        while($run){
+                        $run = true;
+                        $run_index = 0;
+                        while ($run) {
                             $run_index++;
                             $db->direct('delete from `' . $table_row['table_name'] . '_data` where file_id in (select file_id from `' . $table_row['table_name'] . '` where table_name="kandidaten_bilder") limit 10');
                             TualoApplication::result('affected_rows', $db->mysqli->affected_rows);
-                            $run=$db->mysqli->affected_rows!=0;
+                            $run = $db->mysqli->affected_rows != 0;
                             $db->commit();
-                            if ($run_index>50000) $run=false;
-                        }                        
+                            if ($run_index > 50000) $run = false;
+                        }
                         $db->direct('delete from `' . $table_row['table_name'] . '_data` where file_id in (select file_id from `' . $table_row['table_name'] . '` where table_name="kandidaten_bilder") ');
                         $db->direct('delete from `' . $table_row['table_name'] . '` where table_name="kandidaten_bilder"');
-                    }else if ($table_row['table_name'] == 'ds_files_data'){
-
-                    }else{
+                    } else if ($table_row['table_name'] == 'ds_files_data') {
+                    } else {
                         $db->direct('delete from `' . $table_row['table_name'] . '`');
                     }
                     $db->direct('select table_name from `ds` limit 1');
                 }
                 TualoApplication::result('state', __LINE__);
-                TualoApplication::result('seconds',time() - $start);
-                
+                TualoApplication::result('seconds', time() - $start);
+
                 foreach ($table_list as $table_row) {
                     $newData = [];
-                    if ($table_row['table_name'] != 'ds_files_data'){
-                        
-                        foreach($remote_data[$table_row['table_name']]['data'] as $row){
+                    if ($table_row['table_name'] != 'ds_files_data') {
+
+                        foreach ($remote_data[$table_row['table_name']]['data'] as $row) {
                             if (isset($row['__file_name'])) unset($row['__file_name']);
                             if (isset($row['__file_data'])) unset($row['__file_data']);
                             $newData[] = $row;
@@ -126,38 +154,36 @@ class SyncRemote implements IRoute
                 }
 
                 foreach ($table_list as $table_row) {
-                    if ($table_row['table_name'] == 'ds_files_data'){
-                        
-                    }else{
+                    if ($table_row['table_name'] == 'ds_files_data') {
+                    } else {
                         $table = DSTable::instance($table_row['table_name']);
                         $table->insert($remote_data[$table_row['table_name']]['data']);
                         if ($table->error()) throw new \Exception($table->errorMessage());
                     }
                     $db->direct('select table_name from `ds` limit 1');
                 }
-                
+
                 TualoApplication::result('state', __LINE__);
-                TualoApplication::result('seconds',time() - $start);
+                TualoApplication::result('seconds', time() - $start);
                 $db->commit();
 
                 foreach ($table_list as $table_row) {
-                    if ($table_row['table_name'] == 'ds_files_data'){
+                    if ($table_row['table_name'] == 'ds_files_data') {
 
 
-                        $lastCount=-1;
-                        $currentPage=0;
+                        $lastCount = -1;
+                        $currentPage = 0;
                         $tablename = $table_row['table_name'];
 
-                        while($lastCount!=0){
+                        while ($lastCount != 0) {
                             $currentPage++;
-                            $remote_data[$table_row['table_name']]  = APIRequestHelper::query($url . '/ds/' . $tablename . '/read?page='.$currentPage.'&limit=10');
+                            $remote_data[$table_row['table_name']]  = APIRequestHelper::query($url . '/ds/' . $tablename . '/read?page=' . $currentPage . '&limit=10');
                             $lastCount = count($remote_data[$table_row['table_name']]['data']);
-                            foreach($remote_data[$table_row['table_name']]['data'] as $row){
-                                try{
+                            foreach ($remote_data[$table_row['table_name']]['data'] as $row) {
+                                try {
                                     $sql = 'replace into ds_files_data (file_id,data) values ({file_id},{data})';
-                                    $db->direct($sql,$row);
-                                }catch(Exception $e){
-                                    
+                                    $db->direct($sql, $row);
+                                } catch (Exception $e) {
                                 }
                                 $db->direct('select table_name from `ds` limit 1');
                             }
@@ -166,16 +192,15 @@ class SyncRemote implements IRoute
                     }
                 }
                 TualoApplication::result('state', __LINE__);
-                TualoApplication::result('seconds',time() - $start);
+                TualoApplication::result('seconds', time() - $start);
 
                 $db->direct("update stimmzettel set farbe='rgb(101,172,101)' where farbe is null");
                 $db->commit();
                 TualoApplication::result('state', __LINE__);
-                TualoApplication::result('seconds',time() - $start);
+                TualoApplication::result('seconds', time() - $start);
 
-
+                SyncRemote::writeBallotPaperCSS();
                 TualoApplication::result('success', true);
-                
             } catch (\Exception $e) {
                 TualoApplication::result('msg', $e->getMessage());
             }
