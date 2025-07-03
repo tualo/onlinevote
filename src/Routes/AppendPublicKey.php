3d739e31f4cf284051b02cd4d97b77fcb1c8591d
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-07-03 09:02:05 +0200

    fixed used before init

--- src/Routes/AppendPublicKey.php
+++ src/Routes/AppendPublicKey.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Tualo\Office\OnlineVote\Routes;
 
 use Tualo\Office\TualoPGP\TualoApplicationPGP;
@@ -9,18 +10,20 @@ use phpseclib3\Crypt\PublicKeyLoader;
 use phpseclib3\Crypt\Common\PublicKey;
 
 
-class AppendPublicKey implements IRoute{
- 
-    public static function register(){
-        BasicRoute::add('/onlinevote/append/publickey',function(){
+class AppendPublicKey implements IRoute
+{
+
+    public static function register()
+    {
+        BasicRoute::add('/onlinevote/append/publickey', function () {
             App::contenttype('application/json');
-            try{
+            try {
                 $db = App::get('session')->getDB();
-                if (!isset( $_REQUEST['publickey'] )&&!is_string($_REQUEST['publickey'])) throw new \Exception("Ein Attribut wir vermisst");
-                if (!isset( $_REQUEST['name'] )&&!is_string($_REQUEST['publickey'])) throw new \Exception("Ein Attribut wir vermisst");
+                if (!isset($_REQUEST['publickey']) && !is_string($_REQUEST['publickey'])) throw new \Exception("Ein Attribut wird vermisst");
+                if (!isset($_REQUEST['name']) && !is_string($_REQUEST['publickey'])) throw new \Exception("Ein Attribut wird vermisst");
                 $rsaKey = PublicKeyLoader::loadPublicKey($_REQUEST['publickey']);
                 if (!$rsaKey instanceof PublicKey) throw new \Exception("Der Schlüssel kann nicht verwendet werden");
-                if ( $db->singleRow( 'select stoptime from wm_loginpage_settings where starttime > now() and id = 1',[] ) === false) throw new \Exception("Es kann keine Urne mehr angelegt werden");
+                if ($db->singleRow('select stoptime from wm_loginpage_settings where starttime > now() and id = 1', []) === false) throw new \Exception("Es kann keine Urne mehr angelegt werden");
                 $sql = '
                     insert into 
                         pgpkeys 
@@ -42,20 +45,18 @@ class AppendPublicKey implements IRoute{
                         {privatekey}
                     )
                 ';
-        
-                $db->direct($sql,[
-                    'keyid'=>         $_REQUEST['name'],
-                    'fingerprint'=>   $rsaKey->getFingerprint('md5'),
+
+                $db->direct($sql, [
+                    'keyid' =>         $_REQUEST['name'],
+                    'fingerprint' =>   $rsaKey->getFingerprint('md5'),
                     'publickey' =>    (string)$rsaKey,
                     'privatekey' =>   ''
                 ]);
                 App::result('success', true);
-                
- 
-             }catch(\Exception $e){
- 
-                 App::result('msg', $e->getMessage());
-             }
-        },['get','post'],true);
+            } catch (\Exception $e) {
+
+                App::result('msg', $e->getMessage());
+            }
+        }, ['get', 'post'], true);
     }
 }
