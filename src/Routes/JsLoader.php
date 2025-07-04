b8f7582ffa8ce12e86a74dad46af142cbea2794b
Steffen Borchmann <steffen.borchmann@tualo.de>
2025-07-04 10:33:40 +0200

    add security helper for static file serving

--- src/Routes/JsLoader.php
+++ src/Routes/JsLoader.php
@@ -4,16 +4,24 @@ namespace Tualo\Office\OnlineVote\Routes;
 use Tualo\Office\Basic\TualoApplication as App;
 use Tualo\Office\Basic\Route as BasicRoute;
 use Tualo\Office\Basic\IRoute;
+use Tualo\Office\Basic\RouteSecurityHelper;
 
 class JsLoader implements IRoute{
     public static function register(){
         BasicRoute::add('/jsonlinevote/(?P<file>[\w.\/\-]+).js',function($matches){
+            RouteSecurityHelper::serveSecureStaticFile(
+                $matches['file'] . '.js',
+                dirname(__DIR__, 1) . '/js/lazy/',
+                ['js'],
+                ['application/javascript']
+            );
+            /*
             App::contenttype('application/javascript');
             if (file_exists(dirname(__DIR__,1).'/js/lazy/'.$matches['file'].'.js')){
                 App::etagFile( dirname(__DIR__,1).'/js/lazy/'.$matches['file'].'.js', true);
                 BasicRoute::$finished = true;
                 http_response_code(200);
-            }            
+            } */           
         },['get'],false);
 
         BasicRoute::add('/onlinevote/loader.js',function($matches){
@@ -31,7 +39,6 @@ class JsLoader implements IRoute{
                 $content .= file_get_contents( dirname(__DIR__,1).'/'.$item ).PHP_EOL.PHP_EOL;
             }
             App::body( $content );
-            
         },array('get'),false);
 
     }
