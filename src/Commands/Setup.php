3359bbdf45935f58bd4230d3c2e52c3674003382
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-07-30 13:57:21 +0200

    shorten setup

--- src/Commands/Setup.php
+++ src/Commands/Setup.php
@@ -1,66 +1,42 @@
 <?php
+
 namespace Tualo\Office\OnlineVote\Commands;
+
 use Garden\Cli\Cli;
 use Garden\Cli\Args;
 use Tualo\Office\Basic\ISetupCommandline;
 use Tualo\Office\Basic\PostCheck;
-use Tualo\Office\Basic\BaseSetupCommand as BaseSetup;
+use Tualo\Office\CMS\Commands\Setup as BaseSetup;
 
-class Setup extends BaseSetup implements ISetupCommandline{
+class Setup extends BaseSetup
+{
 
-    public static function getCommandName(): string { return 'onlinevote'; }
-    public static function getCommandDescription(): string { return 'perform a complete onlinevote setup'; }
-    public static function setup(Cli $cli){
+    public static function getHeadLine(): string
+    {
+        return 'Online Vote Setup';
+    }
+    public static function getCommandName(): string
+    {
+        return 'onlinevote';
+    }
+    public static function getCommandDescription(): string
+    {
+        return 'perform a complete onlinevote setup';
+    }
+    public static function setup(Cli $cli)
+    {
         $cli->command(self::getCommandName())
             ->description(self::getCommandDescription())
-            ->opt('client', 'only use this client', true, 'string');
+            ->opt('client', 'only use this client', true, 'string')
+            ->opt('user', 'only use this user', true, 'string');
     }
-    public static function run(Args $args) { 
-        $clientName = $args->getOpt('client');
-        if( is_null($clientName) ) $clientName = '';
-        
-        PostCheck::formatPrintLn(['blue'], "Installing all needed for onlinevote module");
-        PostCheck::formatPrintLn(['blue'], "===========================================");
-        
-        $sass_cmd = '';
-        $sencha_cmd = '';
 
-        exec(implode(' ',['which','scss']),$result,$return_code);
-        if ($return_code!==0){
-            PostCheck::formatPrintLn(['red'], "sass not found");
-            
-        }else{
-            PostCheck::formatPrintLn(['green'], "sass found");
-            $sass_cmd = $result[0];
-        }
+    public static function getCommands(Args $args): array
+    {
+        $parentCommands = parent::getCommands($args);
+        return [
+            ...$parentCommands,
 
-        exec(implode(' ',['which','sencha']),$result,$return_code);
-        if ($return_code!==0){
-            PostCheck::formatPrintLn(['red'], "sencha not found");
-        }else{
-            PostCheck::formatPrintLn(['green'], "sencha found");
-            $sencha_cmd = $result[0];
-        }
-
-        if ($sass_cmd==='' || $sencha_cmd===''){
-            PostCheck::formatPrintLn(['red'], "Please install sass and sencha cmd");
-        }else{
-            $installCommands = [
-                'configuration --section scss --key cmd --value '.$sass_cmd,
-                'configuration --section ext-compiler --key sencha_compiler_command --value '. $sencha_cmd,
-                // 'configuration --section ext-compiler --key requires --value "exporter"',
-            ];
-            foreach($installCommands as $cmdString){
-                self::performInstall($cmdString,'');
-            }
-        }
-
-        $installCommands = [
-            'install-sessionsql-bsc-main',
-            'install-sql-sessionviews',
-            'install-sql-bsc-main-ds',
-            'install-sql-bsc-menu',
-            'install-sql-ds-main',
             'install-sql-ds',
             'install-sql-ds-dsx',
             'install-sql-ds-privacy',
@@ -78,12 +54,46 @@ class Setup extends BaseSetup implements ISetupCommandline{
             'import-onlinevote-page',
             // 'compile'
         ];
- 
-        foreach($installCommands as $cmdString){
-            self::performInstall($cmdString,$clientName);
-        }
-
     }
 
-    
+    public static function run(Args $args)
+    {
+        $clientName = $args->getOpt('client');
+        if (is_null($clientName)) $clientName = '';
+
+
+        $sass_cmd = '';
+        $sencha_cmd = '';
+
+        exec(implode(' ', ['which', 'scss']), $result, $return_code);
+        if ($return_code !== 0) {
+            PostCheck::formatPrintLn(['red'], "sass not found");
+        } else {
+            PostCheck::formatPrintLn(['green'], "sass found");
+            $sass_cmd = $result[0];
+        }
+
+        exec(implode(' ', ['which', 'sencha']), $result, $return_code);
+        if ($return_code !== 0) {
+            PostCheck::formatPrintLn(['red'], "sencha not found");
+        } else {
+            PostCheck::formatPrintLn(['green'], "sencha found");
+            $sencha_cmd = $result[0];
+        }
+
+        if ($sass_cmd === '' || $sencha_cmd === '') {
+            PostCheck::formatPrintLn(['red'], "Please install sass and sencha cmd");
+        } else {
+            $installCommands = [
+                'configuration --section scss --key cmd --value ' . $sass_cmd,
+                'configuration --section ext-compiler --key sencha_compiler_command --value ' . $sencha_cmd,
+                // 'configuration --section ext-compiler --key requires --value "exporter"',
+            ];
+            foreach ($installCommands as $cmdString) {
+                self::performInstall($cmdString, '');
+            }
+        }
+
+        parent::run($args);
+    }
 }
