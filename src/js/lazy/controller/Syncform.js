ee66000096e440432c3947dba8a3d57cfaa7d541
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-07-31 14:50:29 +0200

    extend error message

--- src/js/lazy/controller/Syncform.js
+++ src/js/lazy/controller/Syncform.js
@@ -6,30 +6,30 @@ Ext.define('Tualo.OnlineVote.controller.Syncform', {
         let me = this,
             view = me.getView(),
             vm = view.getViewModel(),
-            setup = await fetch('./onlinevote/syncsetup').then((response)=>{return response.json()}),
-            state = await fetch('./onlinevote/state').then((response)=>{return response.json()});
-        if (setup.success==false){
+            setup = await fetch('./onlinevote/syncsetup').then((response) => { return response.json() }),
+            state = await fetch('./onlinevote/state').then((response) => { return response.json() });
+        if (setup.success == false) {
             Ext.toast({
                 html: setup.msg,
                 title: 'Fehler',
                 align: 't',
                 iconCls: 'fa fa-warning'
             });
-        }else{            
+        } else {
             view.getForm().setValues(setup);
             view.enable();
         }
-        if (state.success){
-            vm.set('interrupted',state.interrupted || false);
-            if(state.starttime){
-                vm.set('starttime', Ext.util.Format.date( new Date(state.starttime), 'H:i:s') );
-                vm.set('startdate', Ext.util.Format.date( new Date(state.starttime), 'Y-m-d') );
+        if (state.success) {
+            vm.set('interrupted', state.interrupted || false);
+            if (state.starttime) {
+                vm.set('starttime', Ext.util.Format.date(new Date(state.starttime), 'H:i:s'));
+                vm.set('startdate', Ext.util.Format.date(new Date(state.starttime), 'Y-m-d'));
             }
-            if(state.starttime){
-                vm.set('stoptime', Ext.util.Format.date( new Date(state.stoptime), 'H:i:s') );
-                vm.set('stopdate', Ext.util.Format.date( new Date(state.stoptime), 'Y-m-d') );
+            if (state.starttime) {
+                vm.set('stoptime', Ext.util.Format.date(new Date(state.stoptime), 'H:i:s'));
+                vm.set('stopdate', Ext.util.Format.date(new Date(state.stoptime), 'Y-m-d'));
             }
-        }else{
+        } else {
             Ext.toast({
                 html: state.msg,
                 title: 'Fehler',
@@ -38,37 +38,47 @@ Ext.define('Tualo.OnlineVote.controller.Syncform', {
             });
             view.disable();
         }
-        
-        if ((new Date(state.starttime))<=(new Date())){
+
+        if (state.starttime == null) {
             view.disable();
             Ext.toast({
-                html: "Achtung: Die Wahl wurde bereits gestartet!",
+                html: "Achtung: Der Wahlzeitraum ist nicht gesetzt!",
                 title: 'Fehler',
                 align: 't',
                 iconCls: 'fa fa-warning'
             });
+        } else {
+            if ((new Date(state.starttime)) <= (new Date())) {
+                view.disable();
+                Ext.toast({
+                    html: "Achtung: Die Wahl wurde bereits gestartet!",
+                    title: 'Fehler',
+                    align: 't',
+                    iconCls: 'fa fa-warning'
+                });
+            }
         }
 
 
     },
-    sync:  async function(btn){
+    sync: async function (btn) {
         let me = this,
-        view = me.getView(),
-        x =  view.disable(),
-        syncremote = await fetch('./onlinevote/syncremote').then((response)=>{return response.json()})
-        if (syncremote.success==false){
+            view = me.getView(),
+            x = view.disable(),
+            syncremote = await fetch('./onlinevote/syncremote').then((response) => { return response.json() })
+        if (syncremote.success == false) {
             Ext.toast({
                 html: syncremote.msg,
                 title: 'Fehler',
                 align: 't',
                 iconCls: 'fa fa-warning'
             });
-        }else{
+        } else {
             Ext.toast({
                 html: "Sync erfolgreich",
                 title: 'OK',
                 align: 't',
-            }); 
+            });
         }
         view.enable()
 
